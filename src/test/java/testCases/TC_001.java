0da5a5a72dc79e530a84a3799505cf0ee51bd88d
Pragnasri <pragnasribendi@gmail.com>
2024-09-07 08:04:28 +0530

    Archiect Asian development

--- src/test/java/testCases/TC_001.java
+++ src/test/java/testCases/TC_001.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -40,7 +41,17 @@ public class TC_001 {
 		
 	}
 	
-	//@AfterClass
+	@Test(priority=3)
+	public void print() {
+		System.out.println("print title");
+	}
+	
+	@Test(priority=4)
+	public void print1() {
+		System.out.println("Appium");
+	}
+	
+	@AfterMethod
 	public void tearDown() {
 		driver.quit();
 	}
